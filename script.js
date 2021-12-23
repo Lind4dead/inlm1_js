e04957aaebc965d649e23f9b00867a4b9923deca
unknown <emil.lind@utb.ecutbildning.se>
2021-12-23 13:47:27 +0100

    change user functional, need to work on validation

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const regForm = document.querySelector('#regForm');
 const output = document.querySelector('#userList');
+const saveButton = document.querySelector('#buttonSave');
 const emailInput = document.querySelector('#email');
 const firstNameInput = document.querySelector('#firstName');
 const lastNameInput = document.querySelector('#lastName');
+const idNumber = document.querySelector('#id-number');
 
 
 let users = [
@@ -22,7 +24,7 @@ let users = [
 ]
 
 const validateText = (id) => {
-  
+
   let input = document.querySelector(id);
   let nameError = document.querySelector(id + '-error')
   // nameError.innerText = 'Your name have to be atleast 2 characters long'
@@ -44,20 +46,21 @@ const validateText = (id) => {
 
 const validateMail = (mail) => {
   let regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
-  
+
   let emailError = document.querySelector('#email-error');
   emailError.innerText = 'You have to enter a correct email.'
-  
+
   let dupMail = true;
-  if(users.find(u => u.email === mail.value)) {
+  if (users.find(u => u.email === mail.value)) {
+
     emailError.innerText = 'That email already exists'
     mail.classList.remove('is-valid')
     mail.classList.add('is-invalid');
     mail.focus();
     dupMail = false;
   }
-  
-  if(dupMail){
+  console.log(dupMail);
+  if (dupMail) {
 
     if (regEx.test(mail.value)) {
       mail.classList.remove('is-invalid');
@@ -70,9 +73,9 @@ const validateMail = (mail) => {
       mail.focus();
       return false;
     }
-    
+
   }
-  else{
+  else {
     return false;
   }
 
@@ -95,15 +98,19 @@ const addUserToList = () => {
   users.forEach(user => {
     output.innerHTML += `
     <li class="list-group-item d-flex">
-        <div class="me-auto" id="${user.id}">
-          <div class="fw-bold">${user.firstName} ${user.lastName}</div>
-          <a href="mailto:${user.email}">${user.email}</a>
+        <div class="me-auto d-flex justify-content-between w-100" id="${user.id}">
+          <div>
+            <div class="fw-bold">${user.firstName} ${user.lastName}</div>
+            <a href="mailto:${user.email}">${user.email}</a>
+          </div>  
+          <button type="button" class="btn btn-danger btn-sm" id="buttonChange">Change</button>
         </div>
-        <button class="btn btn-danger btn-sm" id="buttonChange">Change</button>
       </li>
     `
   })
 }
+
+
 
 addUserToList();
 
@@ -117,7 +124,7 @@ email.addEventListener('keyup', () => {
 
 firstNameInput.addEventListener('keyup', () => {
   const list = [...firstNameInput.classList];
-  
+
   if(list.includes('is-invalid'))
   validateText('#' + firstName.id);
 }),
@@ -134,6 +141,37 @@ regForm.addEventListener('submit', e => {
   e.preventDefault();
 
   const errors = [];
+  console.log(e);
+  if(e.submitter.id !== 'buttonSave'){
+
+    for (let i = 0; i < e.currentTarget.length; i++) {
+      if (e.currentTarget[i].type === 'text') {
+        errors[i] = validateText('#' + e.currentTarget[i].id);
+      }
+      else if (e.currentTarget[i].type === 'email') {
+        
+        errors[i] = validateMail(email);
+      }
+    }
+       
+    if (errors.includes(false)) {
+      return;
+    }
+    else {
+      regUser(e);
+      addUserToList();
+      for (let i = 0; i < e.currentTarget.length; i++) {
+        e.currentTarget[i].value = '';
+      }
+    }
+  }
+
+
+  
+else if(e.submitter.id === 'buttonSave'){
+
+  let changeUser = users.find(u => u.id === idNumber.innerText);
+  let index = users.indexOf(changeUser);
 
   for (let i = 0; i < e.currentTarget.length; i++) {
     if (e.currentTarget[i].type === 'text') {
@@ -144,25 +182,80 @@ regForm.addEventListener('submit', e => {
       errors[i] = validateMail(email);
     }
   }
-
-  
-
   if (errors.includes(false)) {
     return;
   }
-  else {
-    regUser(e);
+
+  users[index] = {
+    id: idNumber.innerText,
+    firstName: firstNameInput.value,
+    lastName: lastNameInput.value,
+    email: email.value
+    }
     addUserToList();
     for (let i = 0; i < e.currentTarget.length; i++) {
       e.currentTarget[i].value = '';
     }
+    buttonReg.classList.remove('d-none');
+    buttonSave.classList.add('d-none');
   }
-
 })
 
 output.addEventListener('click', (e) => {
-  e.preventDefault();
+  // e.preventDefault();
 
-  
+  let changeUser = users.find(u => u.id === e.target.parentNode.id);
+
+  if (e.target.type === 'button') {
+
+    firstNameInput.value = changeUser.firstName;
+    lastNameInput.value = changeUser.lastName;
+    email.value = changeUser.email;
+    idNumber.innerText = changeUser.id;
+
+    buttonReg.classList.add('d-none');
+    buttonSave.classList.remove('d-none');
+
+
+  }
+
   // for(let i = 0; i < e.)
 })
+
+// saveButton.addEventListener('submit', (e) => {
+
+//   let changeUser = users.find(u => u.id === idNumber.innerText);
+//   let index = users.indexOf(changeUser);
+//   // console.log(users.indexOf(changeUser));
+
+//   const errors = [];
+//   console.log(errors);
+//   console.log(e);
+//   for (let i = 0; i < e.currentTarget.length; i++) {
+//     if (e.currentTarget[i].type === 'text') {
+//       errors[i] = validateText('#' + e.currentTarget[i].id);
+
+//     }
+//     else if (e.currentTarget[i].type === 'email') {
+
+//       errors[i] = validateMail(email);
+//     }
+//   }
+
+//   // console.log(errors);
+
+//   if (errors.includes(false)) {
+//     return;
+//   }
+
+//   users[index] = {
+//     id: idNumber.innerText,
+//     firstName: firstNameInput.value,
+//     lastName: lastNameInput.value,
+//     email: email.value
+//   }
+
+//   buttonReg.classList.remove('d-none');
+//   buttonSave.classList.add('d-none');
+//   // console.log(users);
+// })
